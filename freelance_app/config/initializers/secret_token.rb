6e0c43b2db1e1f9936408bd098a24897e0a72e9e
Kristine Robison <krobison@gmail.com>
2011-01-22 11:07:39 -0800

    Initial Rails App Framework

---
+++ freelance_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreelanceApp::Application.config.secret_token = '9981c722710c42668ce744c7c4492dfd8e173b7a6e836bb4fde17bbb57f30eaa206ebe17c638f5d386d5311b7a28f5cc9eb568c6569885986415f23b09159e30'
